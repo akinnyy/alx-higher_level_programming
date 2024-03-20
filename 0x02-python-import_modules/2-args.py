d16d3b2affbd979ef5a559ebfd4a8bcfe9ff9823
akinnyy <akinyemiowolabii@gmail.com>
2024-03-20 08:24:24 -0700

    My first tool box for arg manipulation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
         print("{} arguments.".format(len(sys.argv) - 1))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
-        for i in range(len(sys.argv) - 1):
-            print("{}: {}".format(i + 1, (sys.argv[i + 1])))
+    for i in range(len(sys.argv) - 1):
+        print("{}: {}".format(i + 1, (sys.argv[i + 1])))

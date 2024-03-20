e846561ca1d28917220b0ad300c53d6bf010d232
akinnyy <akinyemiowolabii@gmail.com>
2024-03-20 14:57:25 +0100

    arg manipulation

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if len(argv) == 1:
+    print ("{} argument:\n".format (len(argv))
+else:
+           if len(argv) == 0:
+           print ("{} argument.".format (len(argv))
+           else:
+                  for i in range(len(argv)+1):
+                  print ("{} argument:\n".format ((argv[i-1]))
+
+
+

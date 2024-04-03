b7080e55e1b8310570cfdf4db07ed2701a8719d7
akinnyy <akinyemiowolabii@gmail.com>
2024-04-03 08:44:14 -0700

    Error handling and exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -20,4 +20,4 @@ def safe_function(fct, *args):
         return (result)
     except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)i
+        return (None)
